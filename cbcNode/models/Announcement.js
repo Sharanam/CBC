e3d64c8f1a954eafbf64c29bf6a8fb11e27fb37f
38852691 <38852691+Sharanam@users.noreply.github.com>
2022-09-22 08:30:50 +0530

    expiry field in announcement

--- cbcNode/models/Announcement.js
+++ cbcNode/models/Announcement.js
@@ -11,11 +11,21 @@ let announcementSchema = new Schema(
       type: String,
       required: false,
     },
+    expireAt: {
+      type: Date,
+      required: true,
+      default: function () {
+        // 60 seconds from now
+        return new Date().valueOf() + 60000;
+      },
+    },
   },
   {
     timestamps: true,
     collection: "announcements",
   }
 );
+
+announcementSchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
 
 module.exports = mongoose.model("Announcement", announcementSchema);
